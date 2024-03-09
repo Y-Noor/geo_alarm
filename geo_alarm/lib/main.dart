be698b5a334c7444054c1eda55f575ee82c8be6a
Y-Noor <noor_yadallee@hotmail.com>
2024-03-09 22:36:48 +0400

    initi

--- geo_alarm/lib/main.dart
+++ geo_alarm/lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Location location = Location();
+
+  Future<bool> requestPermission() async {
+    final permission = await location.requestPermission();
+    return permission == PermissionStatus.granted;
+  }
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(-20, 57);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
